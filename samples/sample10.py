9a0050e86a1fbaff1d7497ab4c8fc61c4818655d
Matty Herzig <mattyherzig@yahoo.com>
2024-08-13 01:56:39 -0700

    working?

--- samples/sample10.py
+++ samples/sample10.py
@@ -1,4 +1,10 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hello my friend
-        
+        numsMap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numsMap:
+                return [i, numsMap[complement]]
+            numsMap[nums[i]] = i
